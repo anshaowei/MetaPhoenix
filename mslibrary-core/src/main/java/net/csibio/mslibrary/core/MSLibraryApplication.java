a54ee0b1ca90487f78958cc045e86dbe2ad72201
lms <lms19880914@qq.com>
2022-08-07 22:47:58 +0800

    初始化库工程

---
+++ mslibrary-core/src/main/java/net/csibio/mslibrary/core/MSLibraryApplication.java
@@ -0,0 +1,19 @@
+package net.csibio.mslibrary.core;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+@SpringBootApplication
+@EnableAsync
+@EnableCaching
+@EnableScheduling
+@EnableAspectJAutoProxy
+public class MSLibraryApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(MSLibraryApplication.class, args);
+    }
+}
