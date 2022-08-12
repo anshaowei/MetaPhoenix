3930eaad4ae6e85ccc80a289e2367c73146c74f8
anshaowei <anshaowei@gmail.com>
2022-08-12 16:39:40 +0800

    化合物生成算法/库生成算法

---
+++ mslibrary-core/src/main/java/net/csibio/mslibrary/core/controller/TestController.java
@@ -0,0 +1,19 @@
+package net.csibio.mslibrary.core.controller;
+
+import net.csibio.mslibrary.core.parser.hmdb.GnpsParser;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("test")
+public class TestController {
+
+    @Autowired
+    GnpsParser gnpsParser;
+
+    @RequestMapping("/1")
+    public void test1() {
+        gnpsParser.parse("/Users/anshaowei/Downloads/database/ALL_GNPS.json");
+    }
+}
