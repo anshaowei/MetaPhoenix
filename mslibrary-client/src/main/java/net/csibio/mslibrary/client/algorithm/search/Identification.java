5111588492fea2b9ebeab71dee5b95332851a73f
anshaowei <anshaowei@gmail.com>
2022-09-14 21:35:20 +0800

    鉴定打分流程2

--- mslibrary-client/src/main/java/net/csibio/mslibrary/client/algorithm/search/Identification.java
+++ mslibrary-client/src/main/java/net/csibio/mslibrary/client/algorithm/search/Identification.java
@@ -15,6 +15,8 @@ import org.springframework.stereotype.Component;
 import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 @Slf4j
 @Component
@@ -30,43 +32,29 @@ public class Identification {
     SpectrumScorer spectrumScorer;
 
     public Feature identifyFeature(Feature feature, List<String> libraryIds) {
-
-//        //根据化合物进行匹配
-//        for (String libraryId : libraryIds) {
-//            //获取及组装数据
-//            List<CompoundDO> compoundDOS = compoundService.getAllByLibraryId(libraryId);
-//            TreeSet<SimpleCompound> simpleCompounds = new TreeSet<>();
-//            for (CompoundDO compoundDO : compoundDOS) {
-//                for (Adduct adduct : compoundDO.getAdducts()) {
-//                    SimpleCompound simpleCompound = new SimpleCompound();
-//                    simpleCompound.setMz((compoundDO.getMonoMw() + adduct.getMw()) / adduct.getCharge());
-//                    simpleCompound.setCompoundId(compoundDO.getId());
-//                    simpleCompound.setAdduct(adduct);
-//                    simpleCompounds.add(simpleCompound);
-//                }
-//            }
-//            NavigableSet<SimpleCompound> subSet = simpleCompounds.subSet(new SimpleCompound(feature.getMz() - 0.1), true,
-//                    new SimpleCompound(feature.getMz() + 0.1), true);
-//        }
-
         //根据谱图进行匹配
         Double mzTolerance = 0.1;
         List<IdentificationInfo> identificationInfos = new ArrayList<>();
         for (String libraryId : libraryIds) {
             List<SpectrumDO> spectrumDOS = spectrumService.getByPrecursorMz(feature.getMz() - 0.1, feature.getMz() + 0.1, libraryId);
-            for (SpectrumDO spectrumDO : spectrumDOS) {
+            if (spectrumDOS.size() == 0) {
+                continue;
+            }
+            Map<Integer, List<SpectrumDO>> spectrumMap = spectrumDOS.stream().collect(Collectors.groupingBy(SpectrumDO::getMsLevel));
+            List<SpectrumDO> ms2LibSpectra = spectrumMap.get(2);
+            for (SpectrumDO spectrumDO : ms2LibSpectra) {
                 //PrecursorMz打分
                 Double precursorMzScore = Math.abs(spectrumDO.getPrecursorMz() - feature.getMz()) / feature.getMz();
 
                 //谱图相似性打分
-                Spectrum ms1Spectrum = feature.getMs1Spectrum();
-                Spectrum ms2Spectrum = feature.getMs2Spectrum();
-                Spectrum libSpectrum = spectrumDO.getSpectrum();
-                double ms1ForwardScore = spectrumScorer.ms1ForwardScore(ms1Spectrum, libSpectrum, mzTolerance);
-                double ms1ReverseScore = spectrumScorer.ms1ReverseScore(libSpectrum, ms1Spectrum, mzTolerance);
-                double ms2ForwardScore = spectrumScorer.ms2ForwardScore(ms2Spectrum, libSpectrum, mzTolerance);
-                double ms2ReverseScore = spectrumScorer.ms2ReverseScore(libSpectrum, ms2Spectrum, mzTolerance);
-                double similarityScore = (ms1ForwardScore + ms1ReverseScore + ms2ForwardScore + ms2ReverseScore) / 4;
+                Double similarityScore = 0.0;
+                if (feature.getMs2Spectrum() != null) {
+                    Spectrum ms2Spectrum = feature.getMs2Spectrum();
+                    Spectrum libSpectrum = spectrumDO.getSpectrum();
+                    double ms2ForwardScore = spectrumScorer.ms2ForwardScore(ms2Spectrum, libSpectrum, mzTolerance);
+                    double ms2ReverseScore = spectrumScorer.ms2ReverseScore(libSpectrum, ms2Spectrum, mzTolerance);
+                    similarityScore += ms2ForwardScore + ms2ReverseScore;
+                }
 
                 //计算总分
                 double totalScore = precursorMzScore + similarityScore;
@@ -82,13 +70,21 @@ public class Identification {
             }
         }
         identificationInfos.sort(Comparator.comparing(IdentificationInfo::getMatchScore));
+
         //取前10个
         if (identificationInfos.size() > 10) {
-            identificationInfos = identificationInfos.subList(0, 10);
+            identificationInfos = identificationInfos.subList(identificationInfos.size() - 10, identificationInfos.size() - 1);
         }
         feature.setIdentificationInfos(identificationInfos);
 
         return feature;
+    }
+
+    public List<Feature> identifyFeatures(List<Feature> features, List<String> libraryIds) {
+        features.stream().forEach(feature -> {
+            identifyFeature(feature, libraryIds);
+        });
+        return features;
     }
 
 }
