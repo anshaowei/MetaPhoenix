6da26a8035c0c99466da908026cafcf8c8d8e631
anshaowei <anshaowei@gmail.com>
2022-11-17 11:04:10 +0800

    鉴定任务框架

---
+++ mslibrary-core/src/main/java/net/csibio/mslibrary/core/task/IdentificationTask.java
@@ -0,0 +1,10 @@
+package net.csibio.mslibrary.core.task;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+
+@Component("identificationTask")
+@Slf4j
+public class IdentificationTask {
+
+}
