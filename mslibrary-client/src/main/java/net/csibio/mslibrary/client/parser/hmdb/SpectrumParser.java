fff37532d73c262ff6913a7e93a0aa6fb6a56410
anshaowei <anshaowei@gmail.com>
2022-11-18 13:48:42 +0800

    解析HMDB xml格式谱图

--- mslibrary-client/src/main/java/net/csibio/mslibrary/client/parser/hmdb/SpectrumParser.java
+++ mslibrary-client/src/main/java/net/csibio/mslibrary/client/parser/hmdb/SpectrumParser.java
@@ -4,6 +4,12 @@ import lombok.extern.slf4j.Slf4j;
 import net.csibio.mslibrary.client.service.SpectrumService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+import org.w3c.dom.Document;
+import org.w3c.dom.NodeList;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import java.io.File;
 
 @Component
 @Slf4j
@@ -12,8 +18,40 @@ public class SpectrumParser {
     @Autowired
     SpectrumService spectrumService;
 
-    public void parse(String filePath) {
-        log.info("开始执行数据库谱图解析");
+    /**
+     * scan all the xml files in the directory and parse them
+     *
+     * @param directoryPath
+     */
+    public void parse(String directoryPath) {
+        log.info("开始执行文件夹:" + directoryPath + "谱图解析");
+        File file = new File(directoryPath);
+        File[] files = file.listFiles();
+        assert files != null;
+        //search all .xml files
+        for (File f : files) {
+            if (f.isFile() && f.getName().endsWith(".xml")) {
+                log.info("开始解析文件:" + f.getName());
+                parseSingleXML(f.getAbsolutePath());
+            }
+        }
+    }
+
+    /**
+     * parse single xml file to a spectrum
+     *
+     * @param filePath
+     */
+    public void parseSingleXML(String filePath) {
+        try {
+            File file = new File(filePath);
+            DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+            DocumentBuilder builder = factory.newDocumentBuilder();
+            Document document = builder.parse(file);
+            NodeList nodeList = document.getElementsByTagName("spectrum");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
 }
