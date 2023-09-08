3bad2e50c8062dea0c5af1d32c6a1a77054b157f
anshaowei <anshaowei@gmail.com>
2023-09-08 16:20:14 +0800

    Entropy计算方法更新；

--- mslibrary-client/src/main/java/net/csibio/mslibrary/client/algorithm/entropy/Entropy.java
+++ mslibrary-client/src/main/java/net/csibio/mslibrary/client/algorithm/entropy/Entropy.java
@@ -1,20 +1,32 @@
 package net.csibio.mslibrary.client.algorithm.entropy;
 
 import net.csibio.aird.bean.common.Spectrum;
+import net.csibio.mslibrary.client.utils.ArrayUtil;
 
 public class Entropy {
 
     public static double getEntropy(double[] values) {
-        double sum = 0;
-        for (double value : values) {
-            sum += value;
+        if (values.length == 1) {
+            return 0;
         }
+        double sum = ArrayUtil.sum(values);
         double entropy = 0;
-        for (double value : values) {
-            double p = value / sum;
+        boolean isMax = true;
+        for (int i = 0; i < values.length; i++) {
+            if (values[i] != values[0]) {
+                isMax = false;
+            }
+            double p = values[i] / sum;
             entropy += p * Math.log(p);
         }
+        if (isMax) {
+            return Math.log(values.length);
+        }
         return -entropy;
+    }
+
+    public static double getNormalizedEntropy(double[] values) {
+        return getEntropy(values) / Math.log(values.length);
     }
 
     public static double getSpectrumEntropy(Spectrum spectrum) {
